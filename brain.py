5fdea0511fcc26a0f0f2b587dd4902338754851a
dawraibu-sketch <dawraibu@gmail.com>
2026-07-18 18:42:45 +0500

    Update brain.py

--- brain.py
+++ brain.py
@@ -1,2 +1,14 @@
 def reply(message):
-  return "You said: " + message
+    message = message.lower()
+
+    if message == "hello":
+        return "Hello! Nice to meet you."
+
+    elif message == "what is your name":
+        return "I am My-AI."
+
+    elif message == "bye":
+        return "Goodbye!"
+
+    else:
+        return "I don't understand that yet."
