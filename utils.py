4b786abaef27334a28f0cf490327b9315bc9f0c8
dawraibu-sketch <dawraibu@gmail.com>
2026-07-18 19:33:03 +0500

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+def get_greeting():
+    hour = datetime.now().hour
+
+    if hour < 12:
+        return "Good morning"
+
+    elif hour < 18:
+        return "Good afternoon"
+
+    else:
+        return "Good evening"
+
+
+def get_time():
+    return datetime.now().strftime("%I:%M %p")
+
+
+def get_date():
+    return datetime.now().strftime("%d %B %Y")
