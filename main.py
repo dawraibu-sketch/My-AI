90e963f953e7b3069effc35ad95c98552b7d79bf
dawraibu-sketch <dawraibu@gmail.com>
2026-07-19 22:01:45 +0500

    Update main.py

--- main.py
+++ main.py
@@ -63,38 +63,39 @@ while True:
         continue
 
     if user.lower() == "calculate":
-        Menu()
-        choice = input("Enter choice(1/2/3/4/5/6): ")
+        while True:
+            Menu()
+            choice = input("Enter choice(1/2/3/4/5/6): ")
 
-        if choice in ('1', '2', '3', '4','5','6'):
-            num1 = float(input("Enter first number: "))
+            if choice in ('1', '2', '3', '4','5','6'):
+                num1 = float(input("Enter first number: "))
 
-            num2 = float(input("Enter second number: "))
+                num2 = float(input("Enter second number: "))
 
-            if choice == '1':
-                print(num1, "+", num2, "=", add(num1, num2))
+                if choice == '1':
+                    print(num1, "+", num2, "=", add(num1, num2))
 
-            elif choice == '2':
-                print(num1, "-", num2, "=", subtract(num1, num2))
+                elif choice == '2':
+                    print(num1, "-", num2, "=", subtract(num1, num2))
 
-            elif choice == '3':
-                print(num1, "*", num2, "=", multiply(num1, num2))
+                elif choice == '3':
+                    print(num1, "*", num2, "=", multiply(num1, num2))
 
-            elif choice == '4':
-                print(num1, "/", num2, "=", divide(num1, num2))
+                elif choice == '4':
+                    print(num1, "/", num2, "=", divide(num1, num2))
             
-            elif choice == '5':
-                print(num1, "**", num2, "=", power(num1, num2))
+                elif choice == '5':
+                    print(num1, "**", num2, "=", power(num1, num2))
 
-            elif choice == '6':
-                print(num1, "√", num2, "=", round(root(num1, num2), 2))
+                elif choice == '6':
+                    print(num1, "√", num2, "=", round(root(num1, num2), 2))
         
-            next_calculation = input("Let's do next calculation? (yes/no): ")
+                next_calculation = input("Let's do next calculation? (yes/no): ")
 
-            if next_calculation != "yes":
-        
-        else:
-            print("Invalid Input")
+                if next_calculation.lower() != "yes":
+                    break
+            else:
+                print("Invalid Input")
         continue
 
     print("AI:", reply(user))
