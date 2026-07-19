b22d0c00c7f6a94728a535151b96e5212fed966a
dawraibu-sketch <dawraibu@gmail.com>
2026-07-19 20:07:00 +0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,29 +1,35 @@
 def add(x, y):
-  
+    return x + y
+
 def subtract(x, y):
+    return x - y
 
 def multiply(x, y):
+    return x * y
 
 def divide(x, y):
+    return x / y
 
 def power(x, y):
+    return x ** y
 
 def root(x, y):
+    return x ** (1 / y)
   
 def Menu():
   
-      print("Select operation.")
+    print("Select operation.")
 
-      print("1.Add")
+    print("1.Add")
 
-      print("2.Subtract")
+    print("2.Subtract")
 
-      print("3.Multiply")
+    print("3.Multiply")
 
-      print("4.Divide")
+    print("4.Divide")
 
-      print("5.Power")
+    print("5.Power")
 
-      print("6.Root")
+    print("6.Root")
   
-return None
+    return None
