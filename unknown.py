fdca7ec212680f2c5dc8506737f3477a53f6a290
dawraibu-sketch <dawraibu@gmail.com>
2026-07-23 19:52:19 +0500

    Create unknown.py

---
+++ unknown.py
@@ -0,0 +1,12 @@
+unknown = [
+    "I'm not sure I understand that.",
+    "Could you say that another way?",
+    "I don't know how to answer that yet.",
+    "Interesting... can you explain a little more?",
+    "I'm still learning. Try asking differently.",
+    "Sorry, I didn't quite get that.",
+    "Hmm... I don't have a response for that yet.",
+    "Can you rephrase that?",
+    "That's beyond what I know right now.",
+    "I'm still learning new things every day!"
+]
