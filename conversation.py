5a398d89ca99e493da555d8b87c62ad050313fe2
dawraibu-sketch <dawraibu@gmail.com>
2026-08-11 13:44:47 +0500

    Enhance input cleaning by removing punctuation
    Remove trailing question marks and punctuation from user input.

--- conversation.py
+++ conversation.py
@@ -1,7 +1,9 @@
 def clean_input(user):
     user = user.lower().strip()
 
-    if user.endswith("?"):
-        user = user[:-1]
+    punctuation = "!?.,"
+
+    for mark in punctuation:
+        user = user.replace(mark, "")
 
     return user
