ec02aeeb06849fc21ae56cca8d7e96df395086c7
dawraibu-sketch <dawraibu@gmail.com>
2026-08-12 13:34:48 +0500

    Add wellbeing response to reply function

--- brain.py
+++ brain.py
@@ -1,6 +1,7 @@
 import random
 from unknown import unknown
-from greetings import get_greeting
+from greetings import get_greeting, get_wellbeing_response
+
 def reply(message):
     message = message.lower()
 
@@ -8,7 +9,7 @@ def reply(message):
         return get_greeting()
 
     elif message == "how are you":
-        return random.choice(wellbeing_responses)
+        return get_wellbeing_response()
 
     elif message == "what is your name":
         return "I am My-AI."
