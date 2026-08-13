a442dbfaab9a063bf1d85ea3beac15ceabc66a49
dawraibu-sketch <dawraibu@gmail.com>
2026-08-13 18:07:11 +0500

    Add date inquiry responses to conversation.py

--- conversation.py
+++ conversation.py
@@ -52,6 +52,10 @@ def normalize_question(user):
 
     if user in ["what is the time","what time is it","tell me the time","do you know the time","current time"]:
         return "what time is it"
+
+    if user in ["what is today's date","what is the date today","tell me today's date","tell me the date",
+        "what date is it","today's date","current date"]:
+        return "what is today's date"
     
     if user.startswith("what is "):
         return user
